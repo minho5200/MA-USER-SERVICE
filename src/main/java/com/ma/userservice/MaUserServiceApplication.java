edf429faf1b61f1942e3d7dd763faf9b68459902
cdkfk96 <cdkfk96@naver.com>
2024-02-24 16:25:12 +0900

    ara first commit test

--- src/main/java/com/ma/userservice/MaUserServiceApplication.java
+++ src/main/java/com/ma/userservice/MaUserServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MaUserServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MaUserServiceApplication.class, args);
     }
