e380c8a9a179c8e98d3359ac47652a20194562f7
minho <mino.j@sqisoft.com>
2024-06-05 10:38:12 +0900

    JWT 로직 삭제

--- src/main/java/com/ma/userservice/exception/CJwtTokenMalformedException.java
+++
@@ -1,9 +0,0 @@
-package com.ma.userservice.exception;
-
-import javax.naming.AuthenticationException;
-
-public class CJwtTokenMalformedException extends AuthenticationException {
-    public CJwtTokenMalformedException(String message){
-        super(message);
-    }
-}
